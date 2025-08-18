d91eb6ddc9c43de089b92b0d6b41c3b5b18ba185
abdulkader33447 <abdulkader33447@gmail.com>
2025-08-18 17:42:20 +0600

    added reverse function

---
+++ problem-one.js
@@ -0,0 +1,8 @@
+// const text = "programming"
+// const rev = text.split('').reverse().join('')
+// console.log(rev)
+
+const reverseStr = (str) => {
+  return str.split("").reverse().join("");
+};
+console.log(reverseStr("programming"));
