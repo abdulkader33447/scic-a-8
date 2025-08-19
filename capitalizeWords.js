663ce8ea4c5e86581898215f13073b5378dce389
abdulkader33447 <abdulkader33447@gmail.com>
2025-08-19 13:45:07 +0600

    added capitalizeWords function

---
+++ capitalizeWords.js
@@ -0,0 +1,8 @@
+const capitalizeWords = (str) => {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+console.log(capitalizeWords("hello my dear programmer"))
