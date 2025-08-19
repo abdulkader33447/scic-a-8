2738b75b999383698e22eaccc91e04235fd479d0
abdulkader33447 <abdulkader33447@gmail.com>
2025-08-19 14:04:32 +0600

    added printPingPong

---
+++ printPingPong.js
@@ -0,0 +1,14 @@
+const printPingPong = () => {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+};
+printPingPong()
